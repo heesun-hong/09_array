53c4e5b46e3260990fb757b85be3861c932dd9d5
heesun-hong <dukye2001@naver.com>
2019-10-31 16:12:53 +0900

    09_sixth

--- main.c
+++ main.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define ROWS 3
+#define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
-{
-	int i;
-	int grade[5];
-	int score[5];
-	
-	for(i=0;i<5;i++)
-	{
-		grade[i]=rand()%100;
+void printMatrix(int a[][COLS]) {
+	int i, j;
+	for (i=0;i<ROWS;i++){
+		for(j=0;j<COLS;j++)
+			printf("%i ", a[i][j]);
+		printf("\n");	
+	}
+} 
+
+void addMatrix(int a[][COLS], int b[][COLS], int c[][COLS]) {
+	int i, j;
+	for (i=0;i<ROWS;i++)
+		for(j=0;j<COLS;j++)
+			c[i][j] = a[i][j]+b[i][j];	
 	}
 
-	for(i=0;i<5;i++)
-	{
-		score[i]=grade[i];
-	}
+
+int main(void)
+{
+	int a[ROWS][COLS]={//더하기 대상 행렬
+	{2,3,0},
+	{8,9,1},
+	{7,0,5}};
+	int b[ROWS][COLS]={//더하기 대상 행렬
+	{1,0,0},
+	{0,1,0},
+	{0,0,1}};
+	int c[ROWS][COLS]; //덧셈 결과 행렬 
 	
-	for (i=0;i<5;i++)
-	{
-		printf("score[%d] = %d\n", i, score[i]);
-	}
+	addMatrix(a,b,c);
+	printMatrix(c);
 	
-	return 0;
+	return 0; 
 }
+
+	
+
